fe3fa683a6e3f224cb859a67e0c59a3bcf5ced8d
CalebRdgz <calebrodgz@gmail.com>
2023-12-28 14:15:29 -0600

    created POST request at /process-video endpoint which takes in an inputFilePath and outputFilePath and processes that video into 360p. Error handler if user doesnt give inputFilePath and/or outputFilePath. made the port dynamic. created a middleware so that express can handle JSON requests. If ffmpeg completes processing, sends a status 200 OK to client, if not, it sends a 500 interl server error to client.

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,19 +1,39 @@
 import express from "express"
+import Ffmpeg from "fluent-ffmpeg"
 //create local express server
 
 //initialize the express app:
 const app = express();
-
-//default port for express app is port 3000:
-const port = 3000;
+app.use(express.json()); //middleware to handle the JSON requests
 
 //simple route with root path which takes in request and response
-//Define our HTTP GET endpoint:
-app.get("/", (req, res) => {
-  //specify the functionality in the brackets
-  //this is like an anonymous function in typescript
-  res.send("Hello World!")
+//Define our HTTP POST endpoint:
+app.post("/process-video", (req, res) => {
+  //Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath; //from the request body
+  const outputFilePath = req.body.outputFilePath; //same thing for the output
+
+  //error handling technique if inputFilePath/outputFilePath are not given by the user:
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.") //send error 400 - client(user) error
+  }
+  //Actually converting the video:
+  Ffmpeg(inputFilePath) //running imported ffmpeg and call it on the input file
+    .outputOptions("-vf", "scale=-1:360") //(video file, scale video resolution to 360p)
+    //chain this^ with "end" and "error" events:
+    .on("end", () => {
+        //Anonymous Function
+        res.status(200).send("Video processing finished successfully.");
+    })
+    .on("error", (err) => { //anonymous function with the (err) object
+        console.log(`An error occurred: ${err.message}`)
+        res.status(500).send(`Internal Server Error: ${err.message}`) //send internal server error
+    })
+    .save(outputFilePath);
 });
+
+//if process.env.PORT is undefined, set port as 3000:
+const port = process.env.PORT || 3000;
 
 //start our server:
 app.listen(port, () => { // (=> {}) means "execute this code in the brackets"
